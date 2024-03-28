1a96cc33339188a40925026b974a0c8c101d91ae
Alina Ilyukhina <112753331+alinailyukhina@users.noreply.github.com>
2024-03-28 17:25:34 +0300

    worker.js

--- счетчик/worker.js
+++ счетчик/worker.js
@@ -1,23 +1,22 @@
-// Функция для генерации случайных чисел от 1 до 100
-function generateRandomNumbers(length) {
+function getRandomNumber() {
+    return Math.floor(Math.random() * 100) + 1;
+}
+
+function generateRandomNumbers(count) {
     const numbers = [];
-    for (let i = 0; i < length; i++) {
-        numbers.push(Math.floor(Math.random() * 100) + 1); // Генерируем числа от 1 до 100
+    for (let i = 0; i < count; i++) {
+        numbers.push(getRandomNumber());
     }
     return numbers;
 }
 
-// Функция для вычисления среднего значения массива
 function calculateAverage(numbers) {
     const sum = numbers.reduce((acc, curr) => acc + curr, 0);
     return sum / numbers.length;
 }
 
+const numbers = generateRandomNumbers(10000000);
 
-const numbersArray = generateRandomNumbers(10000000); // Генерируем 10 миллионов случайных чисел
+const average = calculateAverage(numbers);
 
-
-const average = calculateAverage(numbersArray);
-
-
-postMessage(average);
+document.getElementById("average").textContent = 'Среднее значение: ' + average;
